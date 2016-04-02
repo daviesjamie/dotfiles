50fe8710cf6b6a4eeafe19eb45e5cbfa0a316fe5
Jamie Davies <jamie@jamiedavies.me>
2016-04-02 10:21:21 +0100

    Add some more basic ZSH aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,3 +13,22 @@ alias l='ls'
 alias la='ls -A'
 alias ll='ls -hl'
 alias lla='ls -Ahl'
+
+# File system tree
+alias .='pwd'
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# Grep with colour
+if echo | grep --color=auto '' > /dev/null 2>&1; then
+    alias grep='grep --color=auto'
+fi
+
+# Clipboard
+if command -v pbcopy > /dev/null 2>&1; then
+    alias pbc='pbcopy'
+    alias pbp='pbpaste'
+elif command -v xclip > /dev/null 2>&1; then
+    alias pbc='xclip -selection clipboard'
+    alias pbp='xclip -selection clipboard -o'
+fi
