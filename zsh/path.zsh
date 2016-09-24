46b64a79456eb0b6630a6100db17ea225e5654f0
Jamie Davies <jamie@jamiedavies.me>
2016-09-24 20:58:53 +0100

    Source rbenv if it's installed

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,3 +4,8 @@ typeset -U path
 [ -d "$DOTFILES/bin" ] && path=("$DOTFILES/bin" $path)
 
 export PATH
+
+# Load rbenv if it's installed
+if command -v rbenv > /dev/null 2>&1; then
+    eval "$(rbenv init -)"
+fi
