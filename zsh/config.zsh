a414d510f8006a8fe71a0f19565377f37aa5436a
Jamie Davies <jamie@jamiedavies.me>
2017-03-21 10:37:35 +0000

    Use emacs-style keybindings at zsh prompt
    I don't promote the use of emacs. I just like being able to press Ctrl+U
    to clear the command line, or Ctrl+R to backwards search the history.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,3 +8,6 @@ export LESS="--quit-if-one-screen --no-init --RAW-CONTROL-CHARS --chop-long-line
 
 # List jobs in long format by default
 setopt LONG_LIST_JOBS
+
+# Use emacs-style keybindings (Ctrl+R for backwards search, Ctrl+U for line clear, etc)
+bindkey -e
