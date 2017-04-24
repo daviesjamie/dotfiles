0ed7d4c9e8e73f30bfca07264678db42a462989d
Jamie Davies <jamie@jamiedavies.me>
2017-04-24 12:15:47 +0100

    Ensure virtualenvwrapper is only sourced if it's installed

--- zsh/virtualenv.zsh
+++ zsh/virtualenv.zsh
@@ -1,3 +1,5 @@
-export WORKON_HOME=$HOME/.venvs
-export PROJECT_HOME=$HOME/Code
-source /usr/local/bin/virtualenvwrapper.sh
+if [[ -f '/usr/local/bin/virtualenvwrapper.sh' ]]; then
+    export WORKON_HOME=$HOME/.venvs
+    export PROJECT_HOME=$HOME/Code
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
