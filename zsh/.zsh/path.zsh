33ead49396e77ff77475beef3f61ca6bb1e67294
Jamie Davies <jamie@jamiedavies.me>
2019-03-18 13:00:43 +0000

    Move loading of rbenv into a separate zsh file

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -3,12 +3,8 @@ typeset -U path
 
 [ -d "$HOME/bin" ] && path=("$HOME/bin" $path)
 
+# rbenv
 [ -d "$HOME/.rbenv/bin" ] && path=("$HOME/.rbenv/bin" $path)
 [ -d "$HOME/.rbenv/plugins/ruby-build/bin" ] && path=("$HOME/.rbenv/plugins/ruby-build/bin" $path)
 
 export PATH
-
-# Load rbenv if it's installed
-if command -v rbenv > /dev/null 2>&1; then
-    eval "$(rbenv init -)"
-fi
---
+++ zsh/.zsh/rbenv.zsh
@@ -0,0 +1,4 @@
+# Load rbenv if it's installed
+if command -v rbenv > /dev/null 2>&1; then
+    eval "$(rbenv init -)"
+fi
