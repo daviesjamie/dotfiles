affa7e5f61b7ef90c82654e668053df17b4dd3ee
Jamie Davies <jamie@jamiedavies.me>
2019-04-19 11:09:59 +0100

    Fix typo in go environment variable config

--- zsh/.zsh/go.zsh
+++ zsh/.zsh/go.zsh
@@ -1,5 +1,5 @@
 # Set up go paths, if go is installed
-if command -v go > /deb/null 2>&1; then
+if command -v go > /dev/null 2>&1; then
     export GOPATH="$HOME/.go"
     export GOROOT="$(brew --prefix golang)/libexec"
 fi
