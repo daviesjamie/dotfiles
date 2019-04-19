86173ea78ed6e78e811f69f529b426c1124b4871
Jamie Davies <jamie@jamiedavies.me>
2019-04-19 11:16:15 +0100

    Hardcode GOROOT, as `brew prefix` is far too slow
    It adds a noticeable delay (1-2 seconds) when opening a new shell!

--- zsh/.zsh/go.zsh
+++ zsh/.zsh/go.zsh
@@ -1,5 +1,5 @@
 # Set up go paths, if go is installed
 if command -v go > /dev/null 2>&1; then
     export GOPATH="$HOME/.go"
-    export GOROOT="$(brew --prefix golang)/libexec"
+    export GOROOT="/usr/local/opt/go/libexec"
 fi
