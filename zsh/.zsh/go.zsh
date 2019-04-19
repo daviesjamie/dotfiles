7193dc25a0a1560dfdb0f2387d98c7e04a2d31c5
Jamie Davies <jamie@jamiedavies.me>
2019-04-19 16:01:11 +0100

    Move GOPATH to recommended $HOME/go

--- zsh/.zsh/go.zsh
+++ zsh/.zsh/go.zsh
@@ -1,5 +1,5 @@
 # Set up go paths, if go is installed
 if command -v go > /dev/null 2>&1; then
-    export GOPATH="$HOME/.go"
+    export GOPATH="$HOME/go"
     export GOROOT="/usr/local/opt/go/libexec"
 fi
