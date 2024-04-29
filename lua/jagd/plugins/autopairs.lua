cfe6c76f1570d100102f103ddbc5d38a32daf3f2
Jamie Davies <jamie@jagd.me>
2024-04-29 13:48:22 +0100

    Add plugin for auto-closing of HTML/JSX tags

--- lua/jagd/plugins/autopairs.lua
+++ lua/jagd/plugins/autopairs.lua
@@ -1,13 +1,20 @@
 return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    dependencies = { "hrsh7th/nvim-cmp" },
-    config = function()
-        require("nvim-autopairs").setup({})
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        dependencies = { "hrsh7th/nvim-cmp" },
+        config = function()
+            require("nvim-autopairs").setup({})
 
-        -- Automatically add `(` after completing a function or method
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        local cmp = require("cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
+            -- Automatically add `(` after completing a function or method
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            local cmp = require("cmp")
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        event = "InsertEnter",
+        opts = {},
+    },
 }
