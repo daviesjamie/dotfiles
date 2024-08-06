b4a1a1ce59eabc50c84786892aec42eabaed8b60
Jamie Davies <jamie@jagd.me>
2024-08-06 18:11:19 +0100

    nvim: Disable some annoying plugins that come with LazyVim

---
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/flash.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
