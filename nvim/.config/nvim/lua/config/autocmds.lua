a3978289f1c298dfd4d827475c90dd94a0d69e8b
Jamie Davies <jamie@jagd.me>
2024-08-06 17:54:16 +0100

    nvim: Bring back in my own options, keymaps and autocmds

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,98 @@
 -- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+
+local autocmd = vim.api.nvim_create_autocmd
+local function augroup(name)
+  return vim.api.nvim_create_augroup("jagd_" .. name, { clear = true })
+end
+
+-- Only show cursor line in current split
+local cursorline_group = augroup("cursorline")
+autocmd("WinLeave", {
+  group = cursorline_group,
+  command = "set nocursorline",
+})
+autocmd("WinEnter", {
+  group = cursorline_group,
+  command = "set cursorline",
+})
+
+-- Briefly highlight yanked range
+autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Remove trailing whitespace on save
+autocmd("BufWritePre", {
+  group = augroup("trailing_whitespace"),
+  command = [[%s/\s\+$//e]],
+})
+
+-- Check if files need to be reloaded after an external change
+autocmd("FocusGained", {
+  group = augroup("reload_files"),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
+-- Turn on spell-checking in some filetypes
+autocmd("FileType", {
+  group = augroup("spelling"),
+  pattern = { "text", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Fix conceallevel for json files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("json_conceal"),
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
+-- Auto-create directory structure when writing a file
+autocmd("BufWritePre", {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-- LazyVim's big file handling
+vim.filetype.add({
+  pattern = {
+    [".*"] = {
+      function(path, buf)
+        return vim.bo[buf]
+            and vim.bo[buf].filetype ~= "bigfile"
+            and path
+            and vim.fn.getfsize(path) > vim.g.bigfile_size
+            and "bigfile"
+          or nil
+      end,
+    },
+  },
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("bigfile"),
+  pattern = "bigfile",
+  callback = function(ev)
+    vim.b.minianimate_disable = true
+    vim.schedule(function()
+      vim.bo[ev.buf].syntax = vim.filetype.match({ buf = ev.buf }) or ""
+    end)
+  end,
+})
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,76 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+local map = function(mode, keys, func, desc)
+  local opts = desc and { desc = desc } or {}
+  vim.keymap.set(mode, keys, func, opts)
+end
+
+map({ "n", "i" }, "<Esc>", "<cmd>nohlsearch<CR><Esc>", "Escape and clear hlsearch")
+
+-- Make jumping with Ctrl D/U stay centered in screen
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Open folds when jumping between search results
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Make yanking/pasting to/from system clipboard a bit easier
+map({ "n", "x" }, "<leader>y", [["+y]], "[Y]ank to system clipboard")
+map("n", "<leader>Y", [["+Y]], "[Y]ank rest of line to system clipboard")
+map({ "n", "x" }, "<leader>d", [["_d]], "[D]elete without losing register contents")
+map("x", "<leader>p", [["_dP]], "[P]aste over visual selection without losing register contents")
+
+-- Stop accidentally pressing Q
+map("n", "Q", "<nop>")
+
+-- Quickly move lines up/down with Alt + j/k
+map("n", "<A-j>", "<cmd>m .+1<CR>==", "Move line down")
+map("n", "<A-k>", "<cmd>m .-2<CR>==", "Move line up")
+map("v", "<A-j>", "<cmd>m '>+1<CR>gv=gv")
+map("v", "<A-k>", "<cmd>m '<-2<CR>gv=gv")
+
+-- Re-select selection after indenting in visual mode
+map("v", ">", ">gv")
+map("v", "<", "<gv")
+
+-- Buffer navigation
+map("n", "[b", vim.cmd.bprevious, "Previous [B]uffer")
+map("n", "]b", vim.cmd.bnext, "Next [B]uffer")
+map("n", "<leader>bd", LazyVim.ui.bufremove, "[D]elete [B]uffer")
+
+-- Quickfix navigation
+map("n", "<leader>xq", vim.cmd.cwindow, "Toggle [Q]uickfix window")
+map("n", "[Q", vim.cmd.cfirst, "First [Q]uickfix entry")
+map("n", "[q", vim.cmd.cprevious, "Previous [Q]uickfix entry")
+map("n", "]q", vim.cmd.cnext, "Next [Q]uickfix entry")
+map("n", "]Q", vim.cmd.clast, "Last [Q]uickfix entry")
+
+-- Location list navigation
+map("n", "<leader>xl", vim.cmd.lwindow, "Toggle [L]ocation list window")
+map("n", "[L", vim.cmd.lfirst, "First [L]ocation list entry")
+map("n", "[l", vim.cmd.lprevious, "Previous [L]ocation list entry")
+map("n", "]l", vim.cmd.lnext, "Next [L]ocation list entry")
+map("n", "]L", vim.cmd.llast, "Last [L]ocation list entry")
+
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+
+-- Diagnostic navigation
+map("n", "<leader>cd", vim.diagnostic.open_float, "[C]ode [D]iagnostic")
+map("n", "[d", diagnostic_goto(true), "Next [D]iagnostic")
+map("n", "]d", diagnostic_goto(false), "Previous [D]iagnostic")
+map("n", "[e", diagnostic_goto(true, "ERROR"), "Next [E]rror")
+map("n", "]e", diagnostic_goto(false, "ERROR"), "Previous [E]rror")
+map("n", "[w", diagnostic_goto(true, "WARN"), "Next [W]arning")
+map("n", "]w", diagnostic_goto(false, "WARN"), "Previous [W]arning")
+
+-- Code formatting
+map({ "n", "v" }, "<leader>cf", function()
+  LazyVim.format({ force = true })
+end, "[C]ode [F]ormat")
