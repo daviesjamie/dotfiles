ce641426983359d4e94ff2c5053865d16ed169b7
Jamie Davies <jamie@jagd.me>
2024-08-21 22:10:59 +0100

    nvim: Add obsidian plugin

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -80,4 +80,28 @@ return {
       }
     end,
   },
+  {
+    "epwalsh/obsidian.nvim",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "notes",
+          path = "~/notes",
+        },
+      },
+      notes_subdir = "0-inbox",
+      daily_notes = {
+        folder = "1-journal",
+        template = "Daily.md",
+      },
+      templates = {
+        folder = "templates",
+      },
+    },
+  },
 }
