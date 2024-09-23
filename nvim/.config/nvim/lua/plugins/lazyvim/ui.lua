f643a5c648702686456d103fe3d57d6080146fee
Jamie Davies <jamie@jagd.me>
2024-09-23 15:29:39 +0100

    nvim: Make starter recent files link show only files in cwd

--- nvim/.config/nvim/lua/plugins/lazyvim/ui.lua
+++ nvim/.config/nvim/lua/plugins/lazyvim/ui.lua
@@ -78,7 +78,11 @@ return {
           section = "Telescope",
         },
         { name = "Find file", action = LazyVim.pick("files", { root = false, hidden = true }), section = "Telescope" },
-        { name = "Recent files", action = LazyVim.pick("oldfiles", { root = false }), section = "Telescope" },
+        {
+          name = "Recent files",
+          action = LazyVim.pick("oldfiles", { root = false, cwd = vim.fn.getcwd() }),
+          section = "Telescope",
+        },
         { name = "Search", action = LazyVim.pick("live_grep", { root = false }), section = "Telescope" },
         { name = "Git commits", action = LazyVim.pick("git_commits", { root = false }), section = "Telescope" },
         { name = "Help", action = LazyVim.pick("help_tags"), section = "Telescope" },
