ce46ba776b05a1842df9fe5ed3ca95663ef4d1d0
Jamie Davies <jamie@jagd.me>
2024-10-09 19:31:27 +0100

    nvim: Pull out augroup helper into utils module too

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+M.augroup = function(name)
+  return vim.api.nvim_create_augroup("jagd_" .. name, { clear = true })
+end
+
 M.map = function(mode, keys, func, desc)
   local opts = desc and { desc = desc } or {}
   vim.keymap.set(mode, keys, func, opts)
