0061de5b34ebb293263926a6f8c924f6230dda42
Jamie Davies <jamie@jagd.me>
2024-11-05 21:20:18 +0000

    nvim: Add more nvim-cmp sources

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,23 +1,43 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
+    "f3fora/cmp-spell",
+    "folke/lazydev.nvim",
     "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-calc",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
+    "nvim-treesitter/nvim-treesitter",
+    "ray-x/cmp-treesitter",
   },
   event = "InsertEnter",
   opts = function()
     local cmp = require("cmp")
+    local context = require("cmp.config.context")
+
+    local in_spell = function()
+      return context.in_treesitter_capture("spell")
+    end
 
     return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "path" },
+        { name = "lazydev" },
       }, {
-        { name = "buffer" },
+        { name = "spell", option = { enable_in_context = in_spell } },
+      }, {
+        { name = "treesitter", keyword_length = 3 },
+        { name = "path" },
+        { name = "calc" },
+      }, {
+        { name = "buffer", keyword_length = 3 },
       }),
       mapping = {
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-e>"] = cmp.mapping({
+          i = cmp.mapping.abort(),
+          c = cmp.mapping.close(),
+        }),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
