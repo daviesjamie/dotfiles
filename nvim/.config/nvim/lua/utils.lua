4dd2a1caa2b5fb4b81dfebf6d0f7bd478d9acb77
Jamie Davies <jamie@jagd.me>
2024-11-05 20:54:26 +0000

    nvim: Add configuration for LSP, nvim-cmp and mason

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -4,6 +4,16 @@ M.augroup = function(name)
   return vim.api.nvim_create_augroup("jagd_" .. name, { clear = true })
 end
 
+M.buffer_map = function(buffer)
+  return function(mode, keys, func, desc)
+    local opts = { buffer = buffer }
+    if desc then
+      opts["desc"] = desc
+    end
+    vim.keymap.set(mode, keys, func, opts)
+  end
+end
+
 M.map = function(mode, keys, func, desc)
   local opts = desc and { desc = desc } or {}
   vim.keymap.set(mode, keys, func, opts)
