ebba1765a9d0f4f3edd4565ef046fdfebd84b8c4
Jamie Davies <jamie@jagd.me>
2024-11-06 12:03:29 +0000

    nvim: Add lspkind to nvim-cmp config to show completion source

--- nvim/.config/nvim/lua/plugins/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -1,12 +1,13 @@
 return {
-  "folke/lazydev.nvim",
-  ft = "lua",
-  cmd = "LazyDev",
-  opts = {
-    library = {
-      { path = "luvit-meta/library", words = { "vim%.uv" } },
-      { path = "LazyVim", words = { "LazyVim" } },
-      { path = "lazy.nvim", words = { "LazyVim" } },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    cmd = "LazyDev",
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
     },
   },
+  { "Bilal2453/luvit-meta", lazy = true },
 }
