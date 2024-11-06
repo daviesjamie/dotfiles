bb43dd66b492f7f2fbe27943f5284a4d28aaf929
Jamie Davies <jamie@jagd.me>
2024-11-06 08:09:03 +0000

    nvim: Setup lazydev.nvim, for better lua LSP configuration

---
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/lazydev.nvim",
+  ft = "lua",
+  cmd = "LazyDev",
+  opts = {
+    library = {
+      { path = "luvit-meta/library", words = { "vim%.uv" } },
+      { path = "LazyVim", words = { "LazyVim" } },
+      { path = "lazy.nvim", words = { "LazyVim" } },
+    },
+  },
+}
