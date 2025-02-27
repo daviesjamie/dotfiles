49185daed3b67c2ae05d4fc5cb1ea8a27d24f7c0
Jamie Davies <jamie@jagd.me>
2025-02-27 09:24:06 +0000

    nvim: Tweak blink.cmp config to be prettier and use fewer snippets

--- nvim/.config/nvim/lua/jagd/plugins/blink.lua
+++ nvim/.config/nvim/lua/jagd/plugins/blink.lua
@@ -1,17 +1,27 @@
 return {
   "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
   version = "*",
   opts = {
-    keymap = { preset = "default" },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
+    completion = {
+      documentation = {
+        auto_show = true,
+        window = {
+          border = "rounded",
+        },
+      },
+      menu = {
+        border = "rounded",
+      },
     },
-    signature = { enabled = true },
+    keymap = { preset = "default" },
+    signature = {
+      enabled = true,
+      window = {
+        border = "rounded",
+      },
+    },
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
     },
   },
-  opts_extend = { "sources.default" },
 }
