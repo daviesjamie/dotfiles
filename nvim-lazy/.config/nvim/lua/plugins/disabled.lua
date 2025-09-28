6fbee2ef28a48af1bf2ef3e1a3863ae38e539078
Jamie Davies <jamie@jagd.me>
2025-09-28 18:51:07 +0100

    nvim-lazy: Disable session persistence

--- nvim-lazy/.config/nvim/lua/plugins/disabled.lua
+++ nvim-lazy/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
   { "bufferline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
   {
     "snacks.nvim",
     opts = {
