2a3ecbb6b0eccca42c5ac263f2c017d9be927189
Jamie Davies <jamie@jagd.me>
2025-09-29 07:27:59 +0100

    nvim-lazy: Disable autocompletion in text and markdown files

--- nvim-lazy/.config/nvim/lua/plugins/blink.lua
+++ nvim-lazy/.config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,9 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    enabled = function()
+      return not vim.tbl_contains({ "markdown", "text" }, vim.bo.filetype)
+    end,
     keymap = {
       preset = "default",
     },
